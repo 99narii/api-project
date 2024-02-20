cf545973ef269e1fb1792be26b97d060fe6914f2
narii <madnarii@gmail.com>
2024-02-20 23:02:58 +0900

    second days

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const API_KEY = `top-headlines`;
-let news = []
+let newsList = []
 const getLatesNews = async ()=>{
     const url = new URL (
         `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/${API_KEY}`
     );
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
-    console.log("rrr", data.articles);
+    newsList = data.articles;
+    render();
+    console.log("rrr", newsList);
 }
+
+const render = () => {
+    const newsHTML = newsList.map(news=>`<section id="news-board">
+    <div class="row news">
+        <div class="col-lg-4">
+            <img class="news-img-size" src=${news.urlToImage} />
+        </div>
+        <div class="col-lg-8">
+            <h2>${news.title}</h2>
+            <p>${news.description}</p>
+            <div>${news.source.name} * ${news.publishedAt}</div>
+        </div>
+    </div>
+</section>
+`).join('');
+    document.getElementById("news-board").innerHTML = newsHTML;
+}
+
 getLatesNews();
